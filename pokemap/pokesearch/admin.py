d162ecc3d2aae9a10ee9106fefa968911e5c1347
Francisco Palm <fpalm@qu4nt.com>
2021-02-06 10:57:45 -0400

    Add search fields, and list display fields to pokesearch admin.

--- pokemap/pokesearch/admin.py
+++ pokemap/pokesearch/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Pokemon, PokemonSpecies, Type, PokemonType
 
-# Register your models here.
+class PokemonTypeInline(admin.TabularInline):
+    model = PokemonType
+    extra = 0
+
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    inlines = (PokemonTypeInline,)
+    list_display = ('id', 'name')
+    search_fields = ('name', )
+
+@admin.register(PokemonSpecies)
+class PokemonSpeciesAdmin(admin.ModelAdmin):
+    search_fields = ('name', )
+    list_display = ('id', 'name')
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    search_fields = ('name', )
+    list_display = ('id', 'name')
+
+@admin.register(PokemonType)
+class PokemonTypeAdmin(admin.ModelAdmin):
+    search_fields = ('pokemon__name', 'type__name')
+    list_display = ('pokemon', 'slot', 'type')
+
