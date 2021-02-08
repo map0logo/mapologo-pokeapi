6a3cceb7983eb0a0d19ae95310fabfce675fbb21
Francisco Palm <fpalm@qu4nt.com>
2021-02-08 09:37:28 -0400

    Search views based directly in requests

--- pokemap/pokesearch/views.py
+++ pokemap/pokesearch/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from django.db.models import Q
 
-# Create your views here.
+import requests
+import requests_cache
+
+from .models import Pokemon
+
+
+requests_cache.install_cache('pokesearch_cache')
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class SearchResultsView(ListView):
+    model = Pokemon
+    template_name = 'search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        object_list = Pokemon.objects.filter(
+            Q(name__icontains=query)
+        )
+
+        for obj in object_list:
+            pokemon = requests.get(f"https://pokeapi.co/api/v2/pokemon/{obj.id}")
+            obj.sprite_url = pokemon.json()["sprites"]["front_default"]
+            obj.height = pokemon.json()["height"]
+            obj.weight = pokemon.json()["weight"]
+        return object_list
