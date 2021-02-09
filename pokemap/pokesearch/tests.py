634a8b26c7b2e31345a5aa78c4bd02d5b6f587c6
Francisco Palm <fpalm@qu4nt.com>
2021-02-09 13:34:08 -0400

    Simple demo test for Pokemon model.

--- pokemap/pokesearch/tests.py
+++ pokemap/pokesearch/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+
+from .models import Pokemon, PokemonType
+
+
+class PokemonTest(TestCase):
+    def create_pokemon(self, id="1000", name="Future Pokemon"):
+        return Pokemon.objects.create(id=id, name=name)
+
+    def test_pokemon_creation(self):
+        p = self.create_pokemon()
+        self.assertTrue(isinstance(p, Pokemon))
+        self.assertEqual(str(p), "Future Pokemon")
